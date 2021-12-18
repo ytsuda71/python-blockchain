c97a6c6f38e2c3b20b767808b43003c5d8b9da3e
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2021-12-18 14:02:02 +0900

    calculate hash of block

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,10 @@
+import hashlib
+import json
 import logging
 import sys
 import time
+
+import utils
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
@@ -10,18 +14,22 @@ class BlockChain(object):
     def __init__(self):
         self.transaction_pool = []
         self.chain = []
-        self.create_block(0, 'init hash')
+        self.create_block(0, self.hash({}))
 
     def create_block(self, nonce, previous_hash):
-        block = {
+        block = utils.sorted_dict_by_key({
             'timestamp': time.time(),
             'transactions': self.transaction_pool,
             'nonce': nonce,
             'previous_hash': previous_hash
-        }
+        })
         self.chain.append(block)
         self.transaction_pool = []
         return block
+
+    def hash(self, block):
+        sorted_block = json.dumps(block, sort_keys=True)
+        return hashlib.sha256(sorted_block.encode()).hexdigest()
 
 
 def pprint(chains):
@@ -35,7 +43,11 @@ def pprint(chains):
 if __name__ == '__main__':
     block_chain = BlockChain()
     pprint(block_chain.chain)
-    block_chain.create_block(5, 'hash 1')
+
+    previous_hash = block_chain.hash(block_chain.chain[-1])
+    block_chain.create_block(5, previous_hash)
     pprint(block_chain.chain)
-    block_chain.create_block(5, 'hash 2')
+
+    previous_hash = block_chain.hash(block_chain.chain[-1])
+    block_chain.create_block(5, previous_hash)
     pprint(block_chain.chain)
---
+++ utils.py
@@ -0,0 +1,6 @@
+import collections
+
+
+def sorted_dict_by_key(unsorted_dict):
+    return collections.OrderedDict(
+        sorted(unsorted_dict.items(), key=lambda d: d[0]))
