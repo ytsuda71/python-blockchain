42250b8b791df0391da11a34211082068ab602d3
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2021-12-20 01:21:06 +0900

    create mining function

--- blockchain.py
+++ blockchain.py
@@ -7,16 +7,20 @@ import time
 import utils
 
 MINING_DIFFICULTY = 3
+MINING_SENDER = 'THE BLOCKCHAIN'
+MINING_REWARD = 1.0
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
 
 
 class BlockChain(object):
 
-    def __init__(self):
+    def __init__(self, blockchain_address=None):
         self.transaction_pool = []
         self.chain = []
         self.create_block(0, self.hash({}))
+        self.blockchain_address = blockchain_address
 
     def create_block(self, nonce, previous_hash):
         block = utils.sorted_dict_by_key({
@@ -61,20 +65,29 @@ class BlockChain(object):
             nonce += 1
         return nonce
 
+    def mining(self):
+        self.add_transaction(
+            sender_blockchain_address=MINING_SENDER,
+            recipient_blockchain_address=self.blockchain_address,
+            value=MINING_REWARD
+        )
+        nonce = self.proof_of_work()
+        previous_hash = self.hash(self.chain[-1])
+        self.create_block(nonce, previous_hash)
+        logger.info({'action': 'mining', 'status': 'success'})
+        return True
+
 
 if __name__ == '__main__':
-    block_chain = BlockChain()
+    my_blockchain_address = 'my_blockchain_address'
+    block_chain = BlockChain(blockchain_address=my_blockchain_address)
     utils.pprint(block_chain.chain)
 
     block_chain.add_transaction('A', 'B', 1.0)
-    previous_hash = block_chain.hash(block_chain.chain[-1])
-    nonce = block_chain.proof_of_work()
-    block_chain.create_block(nonce, previous_hash)
+    block_chain.mining()
     utils.pprint(block_chain.chain)
 
     block_chain.add_transaction('C', 'D', 2.0)
     block_chain.add_transaction('X', 'Y', 3.0)
-    previous_hash = block_chain.hash(block_chain.chain[-1])
-    nonce = block_chain.proof_of_work()
-    block_chain.create_block(nonce, previous_hash)
+    block_chain.mining()
     utils.pprint(block_chain.chain)
