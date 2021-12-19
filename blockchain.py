3e8140f1e0815b9a4dd60911be5f4c9781a0caa3
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2021-12-19 23:49:35 +0900

    adding transaction

--- blockchain.py
+++ blockchain.py
@@ -31,12 +31,29 @@ class BlockChain(object):
         sorted_block = json.dumps(block, sort_keys=True)
         return hashlib.sha256(sorted_block.encode()).hexdigest()
 
+    def add_transaction(self, sender_blockchain_address,
+                        recipient_blockchain_address, value):
+        transaction = utils.sorted_dict_by_key({
+            'sender_blockchain_address': sender_blockchain_address,
+            'recipient_blockchain_address': recipient_blockchain_address,
+            'value': float(value)
+        })
+        self.transaction_pool.append(transaction)
+        return True
+
 
 def pprint(chains):
     for i, chain in enumerate(chains):
         print(f'{"="*25} Chain {i} {"="*25}')
         for k, v in chain.items():
-            print(f'{k:15}{v}')
+            if k == 'transactions':
+                print(k)
+                for d in v:
+                    print(f'{"-"*40}')
+                    for kk, vv in d.items():
+                        print(f'{kk:30}{vv}')
+            else:
+                print(f'{k:15}{v}')
     print(f'{"*"*25}')
 
 
@@ -44,10 +61,13 @@ if __name__ == '__main__':
     block_chain = BlockChain()
     pprint(block_chain.chain)
 
+    block_chain.add_transaction('A', 'B', 1.0)
     previous_hash = block_chain.hash(block_chain.chain[-1])
     block_chain.create_block(5, previous_hash)
     pprint(block_chain.chain)
 
+    block_chain.add_transaction('C', 'D', 2.0)
+    block_chain.add_transaction('X', 'Y', 3.0)
     previous_hash = block_chain.hash(block_chain.chain[-1])
     block_chain.create_block(5, previous_hash)
     pprint(block_chain.chain)
