c520284fc1f01558b8264f87348e30c1054b648c
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2021-12-28 16:41:39 +0900

    Signing transaction

--- Wallet.py
+++ Wallet.py
@@ -1,9 +1,11 @@
+import utils
+
 import base58
 import codecs
 import hashlib
 import binascii
 
-from ecdsa import NIST256p
+from ecdsa import NIST256p, curves
 from ecdsa import SigningKey
 
 
@@ -64,8 +66,40 @@ class Wallet(object):
         return blockchain_address
 
 
+class Transaction(object):
+
+    def __init__(self, sender_private_key, sender_public_key,
+                 sender_blockchain_address, recipient_blockchain_address,
+                 value):
+        self.sender_private_key = sender_private_key
+        self.sender_public_key = sender_public_key
+        self.sender_blockchain_address = sender_blockchain_address
+        self.recipient_blockchain_address = recipient_blockchain_address
+        self.value = value
+
+    def generate_signature(self):
+        sha256 = hashlib.sha256()
+        transaction = utils.sorted_dict_by_key({
+            'sender_blockchain_address': self.sender_blockchain_address,
+            'recipient_blockchain_address': self.recipient_blockchain_address,
+            'value': float(self.value)
+        })
+        sha256.update(str(transaction).encode('utf-8'))
+        message = sha256.digest()
+        private_key = SigningKey.from_string(
+            bytes().fromhex(self.sender_private_key), curve=NIST256p)
+        private_key_sign = private_key.sign(message)
+        signature = private_key_sign.hex()
+
+        return signature
+
+
 if __name__ == '__main__':
     wallet = Wallet()
     print(wallet.private_key)
     print(wallet.public_key)
     print(wallet.blockchain_address)
+    t = Transaction(
+        wallet.private_key, wallet.public_key, wallet.blockchain_address,
+        'B', 1.0)
+    print(t.generate_signature())
