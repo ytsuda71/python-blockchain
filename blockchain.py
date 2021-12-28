3ec973930df3a952df934c641a575d25b05323cc
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2021-12-28 15:13:39 +0900

    calculate total amount

--- blockchain.py
+++ blockchain.py
@@ -77,6 +77,18 @@ class BlockChain(object):
         logger.info({'action': 'mining', 'status': 'success'})
         return True
 
+    def calculate_total_amount(self, blockchain_address):
+        total_amount = 0.0
+        for block in self.chain:
+            for transaction in block['transactions']:
+                value = float(transaction['value'])
+                if blockchain_address == transaction['recipient_blockchain_address']:
+                    total_amount += value
+                if blockchain_address == transaction['sender_blockchain_address']:
+                    total_amount -= value
+
+        return total_amount
+
 
 if __name__ == '__main__':
     my_blockchain_address = 'my_blockchain_address'
@@ -91,3 +103,7 @@ if __name__ == '__main__':
     block_chain.add_transaction('X', 'Y', 3.0)
     block_chain.mining()
     utils.pprint(block_chain.chain)
+
+    print('my', block_chain.calculate_total_amount(my_blockchain_address))
+    print('C', block_chain.calculate_total_amount('C'))
+    print('D', block_chain.calculate_total_amount('D'))
