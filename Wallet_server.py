654edb4222343bbc3f85e06270357a016443b97f
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2021-12-29 17:51:16 +0900

    create Wallet Server

---
+++ Wallet_server.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import jsonify
+from flask import render_template
+
+import Wallet
+
+app = Flask(__name__, template_folder='./templates')
+
+
+@app.route('/')
+def index():
+    return render_template('./index.html')
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=8080,
+                        type=int, help='port to listen')
+    parser.add_argument('-g', '--gw', default='http://127.0.0.1:5000',
+                        type=str, help='blockchain gate')
+    args = parser.parse_args()
+    port = args.port
+    app.config['gw'] = args._get_kwargs
+
+    app.run(host='0.0.0.0', port=port, threaded=True, debug=True)
