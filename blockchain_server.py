89c2bc435492965e0964da2027f4e665657b727e
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2021-12-29 10:31:33 +0900

    create API to get blockchain

--- blockchain_server.py
+++ blockchain_server.py
@@ -6,10 +6,30 @@ import Wallet
 
 app = Flask(__name__)
 
+cache = {}
 
-@app.route('/')
-def hello_world():
-    return 'hello world'
+
+def get_blockchain():
+    cached_blockchain = cache.get('blockchain')
+    if not cached_blockchain:
+        miners_wallet = Wallet.Wallet()
+        cache['blockchain'] = blockchain.BlockChain(
+            blockchain_address=miners_wallet.blockchain_address,
+            port=app.config['port'])
+        app.logger.warning({
+            'private_key': miners_wallet.private_key,
+            'public_key': miners_wallet.public_key,
+            'blockchain_address': miners_wallet.blockchain_address})
+    return cache['blockchain']
+
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    block_chain = get_blockchain()
+    response = {
+        'chain': block_chain.chain
+    }
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
@@ -19,5 +39,7 @@ if __name__ == '__main__':
                         type=int, help='port to listen on')
     args = parser.parse_args()
     port = args.port
+
+    app.config['port'] = port
 
     app.run(host='0.0.0.0', port=port, threaded=True)
