facae28e953d856da60f5527452f57565571b825
Yasuhiro Tsuda <ytsuda@yasuhironoMacBook-Air.local>
2022-01-02 07:47:08 +0900

    create wallet

--- Wallet_server.py
+++ Wallet_server.py
@@ -12,6 +12,17 @@ def index():
     return render_template('./index.html')
 
 
+@app.route('/wallet', methods=['POST'])
+def create_wallet():
+    my_wallet = Wallet.Wallet()
+    response = {
+        'private_key': my_wallet.private_key,
+        'public_key': my_wallet.public_key,
+        'blockchain_address': my_wallet.blockchain_address
+    }
+    return jsonify(response), 200
+
+
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser()
